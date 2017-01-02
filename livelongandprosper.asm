c51bb320aa051e49efd4c63667154322b433abbe
Saxon <saxon.supple@gmail.com>
2017-01-02 19:52:28 +0000

    Update livelongandprosper.asm

--- livelongandprosper.asm
+++ livelongandprosper.asm
@@ -1,3 +1,11 @@
+STD_IN equ 0
+STD_OUT equ 1
+STD_ERR equ 2
+
+SYS_IN equ 0
+SYS_OUT equ 1
+SYS_EXIT equ 60
+
 section .data
 
   text db "Live Long And Prosper",10
@@ -7,12 +15,12 @@ section .text
   global _start
   
 _start:
-  mov rax, 1
-  mov rdi, 1
+  mov rax, SYS_IN
+  mov rdi, STD_OUT
   mov rsi, text
   mov rdx, 22
   syscall
   
-  mov rax, 60
+  mov rax, SYS_EXIT
   mov rdi, 0
   syscall
